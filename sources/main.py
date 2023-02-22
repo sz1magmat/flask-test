de22f04bc33ca3fc90cc31a6f517e316ac441f48
Máté Magyar <sz1magmat@vasvari.org>
2023-02-22 09:38:14 +0100

    asd

--- sources/main.py
+++ sources/main.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Heloasdasdhelos</p>"
+@app.route("/1")
+def hello_world1():
+    return "<p>Hello1</p>"
+
+@app.route("/2")
+def hello_world2():
+    return "<p>Hello2</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
