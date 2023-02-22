0a56adf377a4312026fb6a10ad717198bad45166
Máté Magyar <sz1magmat@vasvari.org>
2023-02-22 09:35:18 +0100

    asd

--- sources/main.py
+++ sources/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Helohelo</p>"
+    return "<p>Helohelos</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
