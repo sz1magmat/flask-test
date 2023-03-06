07e41a5a103358a3621435dada1b6661d5adec46
sz1magmat <sz1magmat@vasvari.org>
2023-03-06 07:39:57 +0100

    test

--- sources/main.py
+++ sources/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from os import getenv
+
 
 app = Flask(__name__)
+
 
 @app.route("/1")
 def hello_world1():
@@ -11,4 +14,4 @@ def hello_world2():
     return "<p>Hello2</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, port=getenv("PORT_TEST"), host="0.0.0.0")
