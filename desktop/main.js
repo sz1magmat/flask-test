dc835b14b2bf6dd7331a2669b43f00fc38db0696
Magyar Máté <98460366+sz1magmat@users.noreply.github.com>
2023-05-03 18:14:14 +0200

    Update main.js

--- desktop/main.js
+++ desktop/main.js
@@ -1,45 +1,33 @@
 const { app, BrowserWindow, screen } = require('electron');
-const windowStateKeeper = require('electron-window-state');
 
-function createWindow()
-{
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-    let mainWindowState = windowStateKeeper(
-    {
-        defaultWidth: width,
-        defaultHeight: height,
-    });
+  const win = new BrowserWindow({
+    show: false,
+    width: width,
+    height: height,
+    icon: 'assets/icon.ico',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
 
-    const win = new BrowserWindow(
-    {
-        show: false,
-        x: mainWindowState.x,
-        y: mainWindowState.y,
-        icon: 'assets/icon.ico',
-        width: mainWindowState.width,
-        height: mainWindowState.height,
+  win.setMenuBarVisibility(false);
+  win.loadURL('https://proj-aums.hu/');
 
-        webPreferences:
-        {
-            nodeIntegration: false,
-            contextIsolation: true,
-        },
-    });
-
-    mainWindowState.manage(win);
-
-    win.setMenuBarVisibility(false);
-    win.loadURL('https://proj-aums.hu/');
-
-    win.once('ready-to-show', () =>
-    { win.show(); });
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () =>
-{ if (process.platform !== 'darwin') app.quit(); });
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
 
-app.on('activate', () =>
-{ if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
